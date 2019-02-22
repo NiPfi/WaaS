448b991505fde287485044a7a087420a00f44b6d
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-02-22 16:44:27 +0100

    Revert "added ng-bootstrap support"
    This reverts commit f3e74b8c69cec2bef1549b61b1fb3bf1345c4759.

--- WaaS/WaaS.Presentation/ClientApp/src/app/app.server.module.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/app.server.module.ts
@@ -4,14 +4,12 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule,
-    NgbModule
+    ModuleMapLoaderModule
   ],
   bootstrap: [AppComponent]
 })
