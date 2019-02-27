e8a4ae4f3a1e47a7f1dedcf949c93d02c6fdd098
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-02-27 23:34:42 +0100

    added migration for changed user model

--- WaaS/WaaS.Infrastructure/Migrations/20190227223419_UserPasswordHashAndSalt.Designer.cs
+++ WaaS/WaaS.Infrastructure/Migrations/20190227223419_UserPasswordHashAndSalt.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WaaS.Infrastructure;
 
 namespace WaaS.Infrastructure.Migrations
 {
     [DbContext(typeof(WaasDbContext))]
-    partial class WaasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190227223419_UserPasswordHashAndSalt")]
+    partial class UserPasswordHashAndSalt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
