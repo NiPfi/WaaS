12d189c05e2ca43a1d1cc299264063e0a9f0ec0b
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-03 11:39:40 +0100

    changed authService to store user

--- WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +13,23 @@ export class AuthService {
   constructor(private jwtHelper: JwtHelperService, private http: HttpClient) { }
 
   public isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
-    return !this.jwtHelper.isTokenExpired(token);
+    return !this.jwtHelper.isTokenExpired(this.parseUser().token);
   }
 
   public get token(): any {
-    return this.jwtHelper.decodeToken(localStorage.getItem('token'));
+    return this.jwtHelper.decodeToken(this.parseUser().token);
   }
 
-  login
+  login(loginUser: User) {
+    return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { loginUser }).pipe(map(user => {
+      if (user && user.token) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+      }
+    }))
+  }
+
+  private parseUser(): User {
+    return JSON.parse(localStorage.getItem('currentUser'));
+  }
 
 }
