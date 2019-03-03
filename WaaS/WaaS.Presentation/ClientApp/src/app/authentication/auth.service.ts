8c8f77474d6434ce5c691e3a8099e30185f375da
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-03 10:52:29 +0100

    added basic methods to auth service

--- WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public jwtHelper: JwtHelperService) { }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
