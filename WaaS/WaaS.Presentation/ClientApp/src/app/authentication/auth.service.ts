92fb4468892de00885ff29f8e98093791c9e9e6e
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-03 11:20:45 +0100

    changed getToken to getter syntax

--- WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public jwtHelper: JwtHelperService) { }
+  constructor(private jwtHelper: JwtHelperService, private http: HttpClient) { }
 
   public isAuthenticated(): boolean {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
   }
 
-  public getToken(): any {
+  public get token(): any {
     return this.jwtHelper.decodeToken(localStorage.getItem('token'));
   }
+
+  login
 
 }
