3318c9cdafcc85c4a802509a2a847d27a4970d6a
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-03 10:57:58 +0100

    Added Auth check to auth guard

--- WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth-guard.guard.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/authentication/auth-guard.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
+
+  constructor (private auth: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+      if(this.auth.isAuthenticated()) {
+        return true;
+      } else {
+        this.router.navigate(['login']);
+        return false;
+      }
   }
-  
+
 }
