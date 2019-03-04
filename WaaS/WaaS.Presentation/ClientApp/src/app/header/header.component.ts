24a5739d689de9d425b895f65d8db1966ad452c4
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-04 10:31:24 +0100

    fixed auth access in template

--- WaaS/WaaS.Presentation/ClientApp/src/app/header/header.component.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../authentication/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
