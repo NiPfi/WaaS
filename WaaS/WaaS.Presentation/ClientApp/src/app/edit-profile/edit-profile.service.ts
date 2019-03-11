93a426640941dd8a07fa1a048cc9209b35c124cd
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-03-11 08:12:27 +0100

    removed unnecessary import

--- WaaS/WaaS.Presentation/ClientApp/src/app/edit-profile/edit-profile.service.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/edit-profile/edit-profile.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../authentication/auth.service';
 import { User } from '../authentication/user';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
