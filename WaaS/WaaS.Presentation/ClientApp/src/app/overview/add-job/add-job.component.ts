433698fa59a357e7e256b05990982f693329eeab
Jonas <jonas@meise.ch>
2019-04-08 15:49:52 +0200

    moved validateAllFormFields to validation service

--- WaaS/WaaS.Presentation/ClientApp/src/app/overview/add-job/add-job.component.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/overview/add-job/add-job.component.ts
@@ -45,7 +45,7 @@ export class AddJobComponent implements OnInit {
 
   createScrapeJob(){
     if (this.addScrapeJobForm.invalid) {
-      this.validateAllFormFields(this.addScrapeJobForm);
+      ValidationService.validateAllFormFields(this.addScrapeJobForm);
       return;
     }
 
@@ -69,17 +69,7 @@ export class AddJobComponent implements OnInit {
       ;
   }
 
-  validateAllFormFields(formGroup: FormGroup) {
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-      if (control instanceof FormControl) {
-        control.markAsTouched({ onlySelf: true });
-        control.markAsDirty({ onlySelf:	true});
-      } else if (control instanceof FormGroup) {
-        this.validateAllFormFields(control);
-      }
-    });
-  }
+  
 
   openAddScrapeJobModal(template: TemplateRef<any>) {
     this.addScrapeJobModalRef = this.modalService.show(template, {});
