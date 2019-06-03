a3153318602f4086221e54b947a575a7e37043ac
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2019-06-03 14:53:44 +0200

    fixed tests

--- WaaS/WaaS.Presentation/ClientApp/src/app/overview/scrape-job-status/scrape-job-status.service.spec.ts
+++ WaaS/WaaS.Presentation/ClientApp/src/app/overview/scrape-job-status/scrape-job-status.service.spec.ts
@@ -1,9 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ScrapeJobStatusService } from './scrape-job-status.service';
 
 describe('ScrapeJobStatusService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      RouterTestingModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: ScrapeJobStatusService = TestBed.get(ScrapeJobStatusService);
